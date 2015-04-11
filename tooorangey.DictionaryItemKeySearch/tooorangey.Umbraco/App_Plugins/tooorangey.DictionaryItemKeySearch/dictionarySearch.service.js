7cb208f5fa81604cfbb0fb1eb4fed7289fe02dba
Marc Goodson <marc@snowmoon.co.uk>
2015-04-11 01:03:01 +0100

    Umbraco 7 dictionary key search dashboard plugin bits

---
+++ tooorangey.DictionaryItemKeySearch/tooorangey.Umbraco/App_Plugins/tooorangey.DictionaryItemKeySearch/dictionarySearch.service.js
@@ -0,0 +1,7 @@
+﻿    angular.module("umbraco.services").factory("dictionarySearchService", function ($http) {  
+        return {
+            SearchDictionaryItems : function(){
+                return $http.get("/umbraco/backoffice/api/DictionarySearch/SearchDictionaryItems/?searchTerm=&languageId=");
+            }
+        };
+    });
---
+++ tooorangey.DictionaryItemKeySearch/tooorangey.Umbraco/App_Plugins/tooorangey.DictionaryItemKeySearch/dictionaryitemkeysearch.controller.js
@@ -0,0 +1,97 @@
+﻿angular.module("umbraco").controller("tooorangey.DictionaryItemKeySearch", function ($scope, $http,angularHelper,notificationsService) {   
+
+    $scope.isSearch = false;
+    $scope.hasResults = false;
+
+    $scope.search = function () {
+   
+        var selectedLanguageId = $scope.selectedLanguage.id;
+     
+        var searchTerm = $scope.searchTerm;
+        $scope.editMode = false;
+  
+        $http.get("/umbraco/backoffice/api/DictionarySearch/SearchDictionaryItems/?searchTerm=" + searchTerm + "&languageId=" + selectedLanguageId).then(function (response) {
+            var matchingItems = response.data;
+    
+            $scope.isSearch = true;
+            $scope.StatusMessage = matchingItems.StatusMessage;
+            $scope.hasResults = matchingItems.HasSearchResults;
+            $scope.dictionaryItems = matchingItems.SearchResults;
+
+        });
+    };
+
+    $scope.load = function () {
+
+        $scope.editMode = false;
+        $scope.SearchTerm = "";
+        //get all languages
+        $http.get("/umbraco/backoffice/api/DictionarySearch/GetAllLanguages").then(function (response) {
+            var allLanguages = response.data;
+       
+            $scope.Languages = allLanguages;
+
+            //get current user
+            //set selected language to be current users language
+            $scope.selectedLanguage = $scope.Languages[0];
+
+        });
+    }
+    $scope.edit = function (id) {
+       
+            //get dictionary item value for the key and selected language
+            var url = '%252Fumbraco%252Fsettings%252FeditDictionaryItem.aspx%253Fid=' +id;
+            top.location = '/umbraco/#/settings/framed/' + url;
+        
+           // $scope.EditMode = true;
+          //  $scope.EditItemText = "the text from the dictionary item"
+
+        };
+    
+
+        $scope.showEditForm = function (editItem) {
+            $scope.editMode = true;
+            $scope.editItemKey = editItem.Key;
+            $scope.editItemText = editItem.Value;
+
+        }
+        $scope.cancelEditForm = function () {
+    
+            $scope.editMode = false;
+            $scope.editItemKey = '';
+            $scope.editItemText = '';
+            $scope.search();
+
+        }
+        $scope.saveItem = function () {
+            var selectedLanguageId = $scope.selectedLanguage.id;
+            var editItemKey = $scope.editItemKey;
+            var editItemText = $scope.editItemText;
+            {msg:'hello word!'}
+            $http.post("/umbraco/backoffice/api/DictionarySearch/UpdateDictionaryItem", { Id: String(selectedLanguageId), key: editItemKey, value: editItemText }
+            ).then(function (response) {
+                //check response status
+        
+        
+                if (response.status == 200){
+                    notificationsService.success('Dictionary Item ' + editItemKey + ' updated to ', editItemText);
+                    $scope.editMode = false;
+                    $scope.editItemKey = '';
+                    $scope.editItemText = '';
+                    $scope.search();
+
+                }
+                else {
+                    notificationsService.error('Error Updating Dictionary Item ' + editItemKey + ': ', 'Language or Dictionary Key was not found');
+                }
+            });
+       };
+    
+
+
+    
+
+   $scope.load();
+
+
+});
