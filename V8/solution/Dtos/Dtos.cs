de7677ce096e6053f9dbc7cd6f3c4f7d81fa50bb
Marc Moriyamas <marc@moriyama.co.uk>
2020-09-23 14:50:01 +0100

    Added V8 ISearchableTree Implementation

---
+++ V8/solution/Dtos/Dtos.cs
@@ -0,0 +1,120 @@
+﻿using NPoco;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Umbraco.Core.Persistence.DatabaseAnnotations;
+
+namespace tooorangey.DictionaryItemKeySearch.Dtos
+{
+    [TableName(TableName)]
+    [PrimaryKey("pk")]
+    [ExplicitColumns]
+    public class DictionaryDto
+    {
+        public const string TableName = Umbraco.Core.Constants.DatabaseSchema.Tables.DictionaryEntry;
+        [Column("pk")]
+        [PrimaryKeyColumn]
+        public int PrimaryKey { get; set; }
+
+        [Column("id")]
+        [Index(IndexTypes.UniqueNonClustered)]
+        public Guid UniqueId { get; set; }
+
+        [Column("parent")]
+        [NullSetting(NullSetting = NullSettings.Null)]
+        [ForeignKey(typeof(DictionaryDto), Column = "id")]
+        [Index(IndexTypes.NonClustered, Name = "IX_" + TableName + "_Parent")]
+        public Guid? Parent { get; set; }
+
+        [Column("key")]
+        [Length(450)]
+        [Index(IndexTypes.NonClustered, Name = "IX_cmsDictionary_key")]
+        public string Key { get; set; }
+
+        [ResultColumn]
+        [Reference(ReferenceType.Many, ColumnName = "UniqueId", ReferenceMemberName = "UniqueId")]
+        [Column]
+        public List<LanguageTextDto> LanguageTextDtos { get; set; }
+    }
+    [TableName(Umbraco.Core.Constants.DatabaseSchema.Tables.DictionaryValue)]
+    [PrimaryKey("pk")]
+    [ExplicitColumns]
+    public class LanguageTextDto
+    {
+        [Column("pk")]
+        [PrimaryKeyColumn]
+        public int PrimaryKey { get; set; }
+
+        [Column("languageId")]
+        [ForeignKey(typeof(LanguageDto), Column = "id")]
+        public int LanguageId { get; set; }
+
+        [Column("UniqueId")]
+        [ForeignKey(typeof(DictionaryDto), Column = "id")]
+        public Guid UniqueId { get; set; }
+
+        [Column("value")]
+        [Length(1000)]
+        public string Value { get; set; }
+
+        [ResultColumn]
+        public string LanguageIsoCode { get; set; }
+
+    }
+    [TableName(TableName)]
+    [PrimaryKey("id")]
+    [ExplicitColumns]
+    public class LanguageDto
+    {
+        public const string TableName = Umbraco.Core.Constants.DatabaseSchema.Tables.Language;
+
+        /// <summary>
+        /// Gets or sets the identifier of the language.
+        /// </summary>
+        [Column("id")]
+        [PrimaryKeyColumn(IdentitySeed = 2)]
+        public short Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ISO code of the language.
+        /// </summary>
+        [Column("languageISOCode")]
+        [Index(IndexTypes.UniqueNonClustered)]
+        [NullSetting(NullSetting = NullSettings.Null)]
+        [Length(14)]
+        public string IsoCode { get; set; }
+
+        /// <summary>
+        /// Gets or sets the culture name of the language.
+        /// </summary>
+        [Column("languageCultureName")]
+        [NullSetting(NullSetting = NullSettings.Null)]
+        [Length(100)]
+        public string CultureName { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the language is the default language.
+        /// </summary>
+        [Column("isDefaultVariantLang")]
+        [Constraint(Default = "0")]
+        public bool IsDefault { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the language is mandatory.
+        /// </summary>
+        [Column("mandatory")]
+        [Constraint(Default = "0")]
+        public bool IsMandatory { get; set; }
+
+        /// <summary>
+        /// Gets or sets the identifier of a fallback language.
+        /// </summary>
+        [Column("fallbackLanguageId")]
+        [ForeignKey(typeof(LanguageDto), Column = "id")]
+        [Index(IndexTypes.NonClustered)]
+        [NullSetting(NullSetting = NullSettings.Null)]
+        public int? FallbackLanguageId { get; set; }
+    }
+
+}
---
+++ V8/solution/SearchableTrees/DictionarySearchableTree.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tooorangey.DictionaryItemKeySearch.Dtos;
+using Umbraco.Core.Scoping;
+using Umbraco.Web.Models.ContentEditing;
+using Umbraco.Web.Trees;
+
+namespace tooorangey.DictionaryItemKeySearch.SearchableTrees
+{
+    public class DictionarySearchableTree : ISearchableTree
+    {
+        private IScopeProvider _scopeProvider;
+        public DictionarySearchableTree(IScopeProvider scopeProvider)
+        {
+            _scopeProvider = scopeProvider;
+        }
+        public string TreeAlias => Umbraco.Core.Constants.Trees.Dictionary;
+
+        public IEnumerable<SearchResultEntity> Search(string query, int pageSize, long pageIndex, out long totalFound, string searchFrom = null)
+        {
+            totalFound = 0;
+            List<SearchResultEntity> searchResults = new List<SearchResultEntity>();
+            if (!string.IsNullOrEmpty(query) && query.Length > 2)
+            {
+                var dictionaryTerms = PerformSearch(query);
+                totalFound = dictionaryTerms.Count();
+                foreach (var dicTerm in dictionaryTerms)
+                {
+                    //is the match on the dictionaryTerm or the value
+                    if (dicTerm.Key.StartsWith(query, System.StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        searchResults.Add(new SearchResultEntity() { Name = dicTerm.Key, Id = dicTerm.PrimaryKey, Key = dicTerm.UniqueId });
+                    }
+                    else
+                    {
+                        var languageTextMatches = new CommaDelimitedStringCollection();
+                        foreach (var languageText in dicTerm.LanguageTextDtos)
+                        {
+                            languageTextMatches.Add(languageText.LanguageIsoCode);
+                        }
+                        searchResults.Add(new SearchResultEntity() { Name = dicTerm.Key + " (" + languageTextMatches.ToString() + ")", Id = dicTerm.PrimaryKey, Key = dicTerm.UniqueId });
+                    }
+
+                }
+            }
+            return searchResults;
+        }
+
+        public IEnumerable<DictionaryDto> PerformSearch(string searchTerm)
+        {
+            using (var scope = _scopeProvider.CreateScope(autoComplete: true))
+            {
+                var sql = "select cd.*, clt.*, ul.LanguageIsoCode from cmsDictionary cd INNER JOIN cmsLanguageText clt on cd.id = clt.UniqueId INNER JOIN umbracoLanguage ul on ul.[id] = clt.languageId WHERE cd.[key] like @0 OR clt.[value] like @1 order by cd.[key]";
+                return scope.Database.FetchOneToMany<DictionaryDto>(x => x.LanguageTextDtos, sql, searchTerm + "%", "%" + searchTerm + "%");
+            }
+        }
+    }
+}
